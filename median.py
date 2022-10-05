38ae9f9fe2e76f989e72982ca7acb3e4bf3a18c6
4v0n <navodhk@hotmail.com>
2022-10-05 22:37:01 +0100

    Return median from list

--- median.py
+++ median.py
@@ -5,8 +5,26 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+
+        numbers.sort
+
+        isEven = (len(numbers) % 2) == 0
+        midPoint = len(numbers) // 2
+
+        if (isEven) :
+            print("is even")
+
+            num1 = numbers[midPoint - 1]
+            num2 = numbers[midPoint]
+
+            print((num1 + num2) / 2)
+
+        else:
+            print("is odd")
+            print(numbers[midPoint])
+
+
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
