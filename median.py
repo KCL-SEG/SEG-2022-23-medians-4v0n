cf8b5e28088a4c1f0d644c1415dca748dcf84c6d
4v0n <navodhk@hotmail.com>
2022-10-05 22:42:17 +0100

    Fix list sorting and removed print statements

--- median.py
+++ median.py
@@ -6,13 +6,12 @@ while True:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
 
-        numbers.sort
+        numbers.sort()
 
         isEven = (len(numbers) % 2) == 0
         midPoint = len(numbers) // 2
 
         if (isEven) :
-            print("is even")
 
             num1 = numbers[midPoint - 1]
             num2 = numbers[midPoint]
@@ -20,7 +19,6 @@ while True:
             print((num1 + num2) / 2)
 
         else:
-            print("is odd")
             print(numbers[midPoint])
 
 
